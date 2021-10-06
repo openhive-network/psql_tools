b1ef750f3257590b27ff366a5f22a4c2a1f5b235
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-10-06 12:47:37 +0200

    start accounts state provider

---
+++ tests/integration/functional/hive_fork_manager/state_providers/accounts_start_state_provider.sql
@@ -0,0 +1,42 @@
+DROP FUNCTION IF EXISTS test_given;
+CREATE FUNCTION test_given()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+VOLATILE
+AS
+$BODY$
+BEGIN
+    PERFORM hive.app_create_context( 'context' );
+END;
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_when;
+CREATE FUNCTION test_when()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    VOLATILE
+AS
+$BODY$
+DECLARE
+    __tables TEXT[];
+BEGIN
+    SELECT hive.start_provider_accounts( 'context' ) INTO __tables;
+
+    ASSERT ( __tables = ARRAY[ 'hive.accounts_context' ]::TEXT[] ), 'Wrong table name';
+END;
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_then;
+CREATE FUNCTION test_then()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+STABLE
+AS
+$BODY$
+BEGIN
+    ASSERT EXISTS ( SELECT FROM information_schema.tables WHERE table_schema='hive' AND table_name  = 'accounts_context' ), 'Accounts table was not created';
+END;
+$BODY$
+;
