4689ec85c1a6fe38fcd5a6656ba0c266ee75f755
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-10-07 10:09:04 +0200

    accounts are ready

--- src/hive_fork_manager/state_providers/accounts.sql
+++ src/hive_fork_manager/state_providers/accounts.sql
@@ -29,6 +29,53 @@ END;
 $BODY$
 ;
 
+CREATE OR REPLACE FUNCTION hive.get_account_from_pow( _pow_operation TEXT )
+    RETURNS TEXT
+    LANGUAGE plpgsql
+    IMMUTABLE
+AS
+$BODY$
+BEGIN
+    RETURN json_extract_path_text( CAST( _pow_operation as json ), 'worker_account' );
+END;
+$BODY$
+;
+
+CREATE OR REPLACE FUNCTION hive.get_account_from_pow2( _pow2_operation TEXT )
+    RETURNS TEXT
+    LANGUAGE plpgsql
+    IMMUTABLE
+AS
+$BODY$
+DECLARE
+    __result TEXT;
+BEGIN
+    SELECT json_extract_path_text( work_arrays.work[2], 'input', 'worker_account' )
+    INTO __result
+    FROM (
+        SELECT array_agg( work.* ) as work
+        FROM json_array_elements( json_extract_path( CAST( _pow2_operation as json ), 'work' ) ) as work
+        ) as work_arrays
+    ;
+
+    RETURN __result;
+END;
+$BODY$
+;
+
+CREATE OR REPLACE FUNCTION hive.get_account_from_accounts_operations( _account_operation TEXT )
+    RETURNS TEXT
+    LANGUAGE plpgsql
+    IMMUTABLE
+AS
+$BODY$
+BEGIN
+    RETURN json_extract_path_text( CAST( _account_operation as json ), 'new_account_name' );
+END;
+$BODY$
+;
+
+
 CREATE OR REPLACE FUNCTION hive.update_state_provider_accounts( _first_block hive.blocks.num%TYPE, _last_block hive.blocks.num%TYPE, _context hive.context_name )
     RETURNS void
     LANGUAGE plpgsql
@@ -50,11 +97,15 @@ BEGIN
 
     EXECUTE format(
         'INSERT INTO hive.accounts_%s( name )
-        SELECT ''test_name'' as name
+        SELECT CASE lower( ot.name )
+            WHEN ''hive::protocol::pow_operation'' THEN hive.get_account_from_pow( ov.body )
+            WHEN ''hive::protocol::pow2_operation'' THEN hive.get_account_from_pow2( ov.body )
+            ELSE hive.get_account_from_accounts_operations( ov.body )
+        END as name
         FROM hive.%s_operations_view ov
         JOIN hive.operation_types ot ON ov.op_type_id = ot.id
         WHERE
-            ARRAY[ lower( ot.name ) ] @> ARRAY[ ''pow'', ''pow2'', ''accountcreate'', ''accountcreatewithdelegation'', ''createclaimedaccount'' ]
+            ARRAY[ lower( ot.name ) ] <@ ARRAY[ ''hive::protocol::pow_operation'', ''hive::protocol::pow2_operation'', ''hive::protocol::account_create_operation'', ''hive::protocol::create_claimed_account_operation'', ''hive::protocol::account_create_with_delegation_operation'' ]
             AND ov.block_num BETWEEN %s AND %s
         ON CONFLICT DO NOTHING'
         , _context, _context, _first_block, _last_block
--- tests/integration/functional/hive_fork_manager/state_providers/accounts_update_state_provider.sql
+++ tests/integration/functional/hive_fork_manager/state_providers/accounts_update_state_provider.sql
@@ -8,11 +8,11 @@ $BODY$
 BEGIN
     INSERT INTO hive.operation_types
     VALUES
-          ( 1, 'pow', FALSE )
-        , ( 2, 'pow2', FALSE )
-        , ( 3, 'accountcreate', FALSE )
-        , ( 4, 'accountcreatewithdelegation', TRUE )
-        , ( 5, 'createclaimedaccount', FALSE )
+          ( 1, 'hive::protocol::pow_operation', FALSE )
+        , ( 2, 'hive::protocol::pow2_operation', FALSE )
+        , ( 3, 'hive::protocol::account_create_operation', FALSE )
+        , ( 4, 'hive::protocol::create_claimed_account_operation', TRUE )
+        , ( 5, 'hive::protocol::account_create_with_delegation_operation', FALSE )
         , ( 6, 'other', FALSE ) -- non creating accounts
     ;
 
@@ -36,16 +36,58 @@ BEGIN
 
     INSERT INTO hive.operations
     VALUES
-           ( 1, 1, 0, 0, 1, '2016-06-22 19:10:21-07'::timestamp, 'pow' )
-         , ( 2, 2, 0, 0, 2, '2016-06-22 19:10:21-07'::timestamp, 'pow2' )
-         , ( 3, 3, 0, 0, 3, '2016-06-22 19:10:21-07'::timestamp, 'accountcreate' )
-         , ( 4, 4, 0, 0, 4, '2016-06-22 19:10:21-07'::timestamp, 'accountcreatewithdelegation' )
-         , ( 5, 5, 0, 0, 5, '2016-06-22 19:10:21-07'::timestamp, 'createclaimedaccount' )
+           ( 1, 1, 0, 0, 1, '2016-06-22 19:10:21-07'::timestamp, '{
+   "worker_account":"account_from_pow",
+   "block_id":"00000449f7860b82b4fbe2f317c670e9f01d6d9a",
+   "nonce":3899,
+   "work":{
+      "worker":"STM7P5TDnA87Pj9T4mf6YHrhzjC1KbPZpNxLWCcVcHxNYXakpoT4F",
+      "input":"ae8e7c677119d22385f8c48026fee7aad7bba693bf788d7f27047f40b47738c0",
+      "signature":"1f38fe9a3f9989f84bd94aa5bbc88beaf09b67f825aa4450cf5105d111149ba6db560b582c7dbb026c7fc9c2eb5051815a72b17f6896ed59d3851d9a0f9883ca7a",
+      "work":"000e7b209d58f2e64b36e9bf12b999c6c7af168cc3fc41eb7f8a4bf796c174c3"
+   },
+   "props":{
+      "account_creation_fee":{
+         "amount":"100000",
+         "precision":3,
+         "nai":"@@000000021"
+      },
+      "maximum_block_size":131072,
+      "hbd_interest_rate":1000
+   }
+}' ) --pow
+         , ( 2, 2, 0, 0, 2, '2016-06-22 19:10:21-07'::timestamp, '{
+    "work": [
+      0,
+      {
+        "input": {
+          "worker_account": "account_from_pow2",
+          "prev_block": "003ea604345523c344fbadab605073ea712dd76f",
+          "nonce": "1052853013628665497"
+        },
+        "pow_summary": 3817904373
+      }
+    ],
+    "props": {
+      "account_creation_fee": {
+        "amount": "1",
+        "precision": 3,
+        "nai": "@@000000021"
+      },
+      "maximum_block_size": 131072,
+      "hbd_interest_rate": 1000
+    }
+  }' ) --pow2
+         , ( 3, 3, 0, 0, 3, '2016-06-22 19:10:21-07'::timestamp, '{"new_account_name": "account_from_create_account"}' )
+         , ( 4, 4, 0, 0, 4, '2016-06-22 19:10:21-07'::timestamp, '{"new_account_name": "account_from_create_claimed_account"}' )
+         , ( 5, 5, 0, 0, 5, '2016-06-22 19:10:21-07'::timestamp, '{"new_account_name": "account_from_create_claimed_account_del"}' )
          , ( 6, 5, 0, 1, 6, '2016-06-22 19:10:21-07'::timestamp, 'other' )
     ;
 
     PERFORM hive.app_create_context( 'context' );
     PERFORM hive.start_provider_accounts( 'context' );
+    UPDATE hive.contexts SET current_block_num = 6, irreversible_block = 6;
+
 END;
 $BODY$
 ;
@@ -58,7 +100,7 @@ CREATE FUNCTION test_when()
 AS
 $BODY$
 BEGIN
-    PERFORM hive.update_state_provider_accounts( 2, 4, 'context' );
+    PERFORM hive.update_state_provider_accounts( 1, 5, 'context' );
 END;
 $BODY$
 ;
@@ -71,7 +113,13 @@ STABLE
 AS
 $BODY$
 BEGIN
-    ASSERT EXISTS ( SELECT FROM information_schema.tables WHERE table_schema='hive' AND table_name  = 'accounts_context' ), 'Accounts table was not created';
+    ASSERT EXISTS ( SELECT * FROM hive.accounts_context WHERE name = 'account_from_pow' ), 'account_from_pow not created';
+    ASSERT EXISTS ( SELECT * FROM hive.accounts_context WHERE name = 'account_from_pow2' ), 'account_from_pow2 not created';
+    ASSERT EXISTS ( SELECT * FROM hive.accounts_context WHERE name = 'account_from_create_account' ), 'account_from_create_account not created';
+    ASSERT EXISTS ( SELECT * FROM hive.accounts_context WHERE name = 'account_from_create_claimed_account' ), 'account_from_create_claimed_account not created';
+    ASSERT EXISTS ( SELECT * FROM hive.accounts_context WHERE name = 'account_from_create_claimed_account_del' ), 'account_create_with_delegation_operation not created';
+
+    ASSERT ( SELECT COUNT(*) FROM hive.accounts_context ) = 5, 'Wrong number of accounts';
 END;
 $BODY$
 ;
